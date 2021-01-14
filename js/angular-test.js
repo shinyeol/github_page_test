6394bfe886215c84f203554235acd5ee1c38ba4d
SHINYEOL <lsl1117@hanmail.net>
2021-01-14 16:12:35 +0900

    modified logic a creating post in main page

--- js/angular-test.js
+++ js/angular-test.js
@@ -1,61 +1,43 @@
 function init() {
-    console.log("111 do init in angular.js");
-    
-    console.log(data);
-    
+ 
     // bind data for memus
     var menuList = data.menus;
-    // var manuTmp = [];
+
+    // bind data for posts
+    var postList = data.posts;
+
+    // var postTmp = [];
     
-    // for(var i = 0; i < menuList.length; i++) {
-    //     manuTmp += '<li class="nav-item">'
-    //         + '<a class="nav-link" href="' + menuList[i].url + '">'+ menuList[i].name +'</a>'
-    //         + '</li>';
+    // for(var i = 0; i < postList.length; i++) {
+    //     postTmp += '<div class="post-preview">'
+    //             + '<a href="'+ postList[i].url +'">'
+    //             + '<h2 class="post-title">'
+    //             + postList[i].title
+    //             + '</h2>'
+    //             + '<h3 class="post-subtitle">'
+    //             + postList[i].subtitle
+    //             + '</h3>'
+    //             + '</a>'
+    //             + '<p class="post-meta">Posted by '
+    //             + '<a href="#">'+ postList[i].author +'</a></p>'
+    //             + '</div><hr>';
         
     // }
     
-    // bind data for posts
-    var postList = data.posts;
-    var postTmp = [];
-    
-    for(var i = 0; i < postList.length; i++) {
-        postTmp += '<div class="post-preview">'
-                + '<a href="'+ postList[i].url +'">'
-                + '<h2 class="post-title">'
-                + postList[i].title
-                + '</h2>'
-                + '<h3 class="post-subtitle">'
-                + postList[i].subtitle
-                + '</h3>'
-                + '</a>'
-                + '<p class="post-meta">Posted by '
-                + '<a href="#">'+ postList[i].author +'</a></p>'
-                + '</div><hr>';
-        
-    }
-    
-    
-    
     var app = angular.module("myApp", []);
     
-    // controller
+    // controller for create navi
     app.controller('namesCtrl', function($scope) {
       $scope.menus = menuList;
+      $scope.posts = postList;
     });
     
-    
-    // directive
-    // app.directive("menuTmp", function() {
+    // directive for post
+    // app.directive("postTmp", function() {
     //   return {
-    //     template : manuTmp
+    //     template : postTmp
     //   };
     // });
-    
-    app.directive("postTmp", function() {
-      return {
-        template : postTmp
-      };
-    });
 }
 
 init();
